61626b6280d199925b9626545e6ca6d3dc064656
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-03-24 23:54:12 +0530

    add react-bootstrap navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,48 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { useStateValue } from "../../StateProvider";
 import { auth } from "../../firebase/firebase.utils";
 import "./header.scss";
-import CustomButton from "../custom-button/custom-button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function Header() {
+function Header(props) {
   const [{ user }] = useStateValue();
   return (
     <div className="header">
-      <Link to="/" className="logo-container">
-        <Logo className="logo" />
-      </Link>
-      <div className="options">
-        <Link className="option" to="/about">
-          HOME
-        </Link>{" "}
-        <Link className="option" to="/about">
-          ABOUT US
-        </Link>{" "}
-        <Link className="option" to="/about">
-          FEATURES
-        </Link>
-        <Link className="option" to="/about">
-          CONTACT US
-        </Link>
-        {user?.currentUser ? (
-          <div
-            className="option"
-            onClick={() => {
-              auth.signOut();
-            }}
-          >
-            SIGN OUT
-          </div>
-        ) : (
-          <Link className="option" to="/login">
-            SIGN IN
-          </Link>
-        )}
-        <CustomButton>Hello</CustomButton>
-        <p>{user?.currentUser?.displayName}</p>
-      </div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand href="/">Bae.Area Club</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/about">Features</Nav.Link>
+            <Nav.Link href="/about">Contact Us</Nav.Link>
+          </Nav>
+          <Nav>
+            {user?.currentUser ? (
+              <Navbar.Text>
+                Signed in as:{" "}
+                <span
+                  onClick={() => {
+                    auth.signOut();
+                  }}
+                  className="sign-out"
+                >
+                  {user?.currentUser?.displayName}
+                </span>
+              </Navbar.Text>
+            ) : (
+              <Nav.Link href="/login">Sign In</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
