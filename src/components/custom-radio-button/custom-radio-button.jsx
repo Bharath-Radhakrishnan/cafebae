0f69821b5d4df346b6d3b1d2c075a5a4a02ef5a0
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-04 17:24:27 +0530

    Customize custom radio button

--- src/components/custom-radio-button/custom-radio-button.jsx
+++ src/components/custom-radio-button/custom-radio-button.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./custom-radio-button.scss";
 function CustomRadioButton() {
+  const [selected, setSelected] = useState("");
+  const handleClick = (e) => {
+    const { name } = e.target;
+    setSelected(name);
+    console.log("clicked", e.target, name);
+    console.log({ selected });
+  };
+
+  const style = {
+    boxShadow:
+      "inset 0 0 0 4px #1597ff, 0 15px 15px -10px rgba(darken(#1597ff, 10%), 0.375)",
+  };
   return (
     <div className="radio-buttons">
-      <input type="radio" id="fat" name="fatfit" />
-      <input type="radio" id="fit" name="fatfit" />
-      <input type="radio" id="other" name="fatfit" />
+      <input type="radio" id="fat" name="fat" onClick={handleClick} />
+      <input type="radio" id="fit" name="fit" onClick={handleClick} />
+      <input type="radio" id="other" name="other" onClick={handleClick} />
 
       <div className="button-label-container">
-        <label for="fat">Male</label>
+        <label for="fat" style={style}>
+          Male
+        </label>
         <label for="fit">Female</label>
         <label for="other">Other</label>
       </div>
--- src/pages/registration/registration-1.jsx
+++ src/pages/registration/registration-1.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-
+import CustomRadioButton from "../../components/custom-radio-button/custom-radio-button";
+import "./registration.scss";
 function Registration1() {
   return (
-    <div>
-      Register <h1>Let's Start with the Basics</h1>
+    <div className="registration-container">
+      <h1>Let's Start with the Basics</h1>
+      <form className="input-form">
+        <div className="name">
+          <label for="name">What is your name?</label>
+          <input type="text" placeholder="name" name="name" id="name" />
+          <label for="dob">Date of Birth</label>
+          <input type="date" placeholder="dob" name="dob" id="dob" />
+        </div>
+
+        {/* <div className="gender-selection">
+          <p>Please select your gender:</p>
+          <label for="male">Male</label>
+          <input type="radio" id="male" name="gender" value="male" />
+          <label for="female">Female</label>
+          <input type="radio" id="female" name="gender" value="female" />
+          <label for="other">Other</label>
+          <input type="radio" id="other" name="gender" value="other" />
+        </div> */}
+        <CustomRadioButton />
+      </form>
     </div>
   );
 }
