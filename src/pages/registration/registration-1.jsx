d39ff1e0bcb24bfcaf77e51f34d0ecdd20dd4fd6
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-06 10:43:17 +0530

    import custom radio button

--- src/pages/registration/registration-1.jsx
+++ src/pages/registration/registration-1.jsx
@@ -12,16 +12,7 @@ function Registration1() {
           <label for="dob">Date of Birth</label>
           <input type="date" placeholder="dob" name="dob" id="dob" />
         </div>
-
-        {/* <div className="gender-selection">
-          <p>Please select your gender:</p>
-          <label for="male">Male</label>
-          <input type="radio" id="male" name="gender" value="male" />
-          <label for="female">Female</label>
-          <input type="radio" id="female" name="gender" value="female" />
-          <label for="other">Other</label>
-          <input type="radio" id="other" name="gender" value="other" />
-        </div> */}
+        <label>Gender</label>
         <CustomRadioButton />
       </form>
     </div>
