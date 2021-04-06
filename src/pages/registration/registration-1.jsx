1de61ecce3784ed3053ebfb3a2cb99181c9bf63a
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-06 21:48:45 +0530

    gender selection values

--- src/pages/registration/registration-1.jsx
+++ src/pages/registration/registration-1.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CustomRadioButton from "../../components/custom-radio-button/custom-radio-button";
 import "./registration.scss";
 function Registration1() {
+  const items = ["Male", "Female", "Other"];
+  const [selected, setSelected] = useState("");
+  const handleClick = (e) => {
+    const { name } = e.target;
+    setSelected(name);
+  };
   return (
     <div className="registration-container">
       <h1>Let's Start with the Basics</h1>
       <form className="input-form">
         <div className="name">
-          <label for="name">What is your name?</label>
+          <label htmlFor="name">What is your name?</label>
           <input type="text" placeholder="name" name="name" id="name" />
-          <label for="dob">Date of Birth</label>
+          <label htmlFor="dob">Date of Birth</label>
           <input type="date" placeholder="dob" name="dob" id="dob" />
         </div>
         <label>Gender</label>
-        <CustomRadioButton />
+        <CustomRadioButton
+          items={items}
+          selected={selected}
+          onClick={handleClick}
+        />
       </form>
     </div>
   );
