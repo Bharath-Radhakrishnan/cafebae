a4bb4fd8d320fbad9ca5dc89226972434c1f53ed
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-21 20:35:02 +0530

    use firebase hooks

--- src/components/protected-route/protected-route.jsx
+++ src/components/protected-route/protected-route.jsx
@@ -1,20 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Redirect, Route } from "react-router";
 import { auth } from "../../firebase/firebase.utils";
 import Loader from "react-loader-spinner";
-import { useStateValue } from "../../StateProvider";
-import AuthenticationService from "../../services/authenticationservice";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function ProtectedRoute({ component: Component, ...rest }) {
-  const isAuthenticated = AuthenticationService.getUserCredentials() !== null;
   const [user, loading] = useAuthState(auth);
 
   if (loading) return <div>Loading</div>;
   if (!user) return <Redirect to={{ pathname: "/" }} />;
   return (
     <Route>
-      <Component />;
+      <Component />
     </Route>
   );
 }
