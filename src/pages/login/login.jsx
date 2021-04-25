f540fdde23b018861c7f24f1c9973c261da99a03
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-25 19:38:35 +0530

    useHistory to redirect to dashboard after login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+import { useHistory } from "react-router-dom";
+
 import { Redirect } from "react-router-dom";
 import { useStateValue } from "../../StateProvider";
 
 function Login() {
   const [{ user }] = useStateValue();
-  if (user.currentUser) {
-    return <Redirect to={"/dashboard"} />;
-  }
+  const history = useHistory();
+  const signIn = async () => {
+    const auth = await signInWithGoogle();
+    if (auth) {
+      history.push("/dashboard");
+    }
+  };
   return (
     <div className="login">
       <h1>Join Bae.Area Club</h1>
       <h3>Sign in with your Google Account</h3>
-      <button onClick={signInWithGoogle}>signInWithGoogle</button>
+      <button onClick={signIn}>signInWithGoogle</button>
     </div>
   );
 }
