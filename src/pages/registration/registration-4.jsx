adb7fbd17e89dee3122bb3ac0282f41b1e8e1b02
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-27 23:24:30 +0530

    add compo

--- src/pages/registration/registration-4.jsx
+++ src/pages/registration/registration-4.jsx
@@ -1,12 +1,43 @@
 import "./registration.scss";
 import { useHistory } from "react-router";
 import { useStateValue } from "../../StateProvider";
+import CustomRadioButton from "../../components/custom-radio-button/custom-radio-button";
+import { useState } from "react";
 
 function Registration4() {
   const [{ register }, dispatch] = useStateValue();
+  const items = [
+    "6 months",
+    "6 months - 1 yr",
+    "1 yr - 2 yr",
+    "2+ yrs",
+    "No plans as of now",
+  ];
+  const [selected, setSelected] = useState("");
   const history = useHistory();
   console.log({ register });
-  return <div className="registration-form"></div>;
+
+  //------------------Methods---------------
+  const handleSelection = (e) => {
+    const { name } = e.target;
+    setSelected(name);
+  };
+
+  //-----------Render-----------------------
+  return (
+    <div className="registration-form">
+      <form className="input-form">
+        <div className="name">
+          <label>When do you plan to settle on</label>
+          <CustomRadioButton
+            items={items}
+            selected={selected}
+            onClick={handleSelection}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Registration4;
