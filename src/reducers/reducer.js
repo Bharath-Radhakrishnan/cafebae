a94222d5a7df42b99d6e56f558d14cd4b75ed90c
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-27 22:05:55 +0530

    register return update

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,31 +2,12 @@ import { registerActionTypes } from "./register/register.types";
 import { userActionTypes } from "./user/user.types";
 
 export const initialState = {
-  // basket: [],
   user: { currentUser: null },
   register: {},
 };
 
 const reducer = (state, action) => {
-  console.log({ state, action });
   switch (action.type) {
-    // case "ADD_TO_BASKET":
-    //   return { ...state, basket: [...state.basket, action.item] };
-    // case "EMPTY_BASKET":
-    //   return { ...state, basket: [] };
-    // case "REMOVE_FROM_BASKET":
-    //   const index = state.basket.findIndex(
-    //     (basketItem) => basketItem.id === action.id
-    //   );
-    //   let newBasket = [...state.basket];
-    //   if (index >= 0) {
-    //     newBasket.splice(index, 1);
-    //   } else {
-    //     console.warn("Item not found in basket");
-    //   }
-    //   return { ...state, basket: newBasket };
-    // default:
-    //   return state;
     case userActionTypes.SET_CURRENT_USER:
       return {
         ...state,
@@ -35,7 +16,7 @@ const reducer = (state, action) => {
     case registerActionTypes.ADD_REGISTER_DATA:
       return {
         ...state,
-        register: action.payload,
+        register: { ...state.register, ...action.payload },
       };
   }
 };
