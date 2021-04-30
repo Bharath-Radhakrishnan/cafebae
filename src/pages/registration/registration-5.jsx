25867c2b9f10882453814cc3d23e561b0bd459ae
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-30 19:38:13 +0530

    change radio button label

--- src/pages/registration/registration-5.jsx
+++ src/pages/registration/registration-5.jsx
@@ -4,54 +4,42 @@ import { useStateValue } from "../../StateProvider";
 import CustomRadioButton from "../../components/custom-radio-button/custom-radio-button";
 import genderList from "../../data/gender";
 import { useState } from "react";
-import { registerActionTypes } from "../../reducers/register/register.types";
-import { useCollection } from "react-firebase-hooks/firestore";
 import { auth, firestore } from "../../firebase/firebase.utils";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function Registration5() {
-
-//----------------------Hooks---------------------------------
+  //----------------------Hooks---------------------------------
   const [{ register }, dispatch] = useStateValue();
   const history = useHistory();
-  const [user] = useAuthState(auth);
-  // const [userSnapshot, loading, error] = useCollection( firestore.collection("users").doc(user.uid));
   const items = genderList;
-//-------------------State-Variables-------------------------
-  const [{age,occupation},setData] = useState({age:"",occupation:""})
-  const [selected,setSelected] = useState("")
-//--------------------Methods--------------------------------
-  const handleSelection=(e)=>{
-    const {value } = e.target;
+  //-------------------State-Variables-------------------------
+  const [{ age, occupation }, setData] = useState({ age: "", occupation: "" });
+  const [selected, setSelected] = useState("");
+  //--------------------Methods--------------------------------
+  const handleSelection = (e) => {
+    const { value } = e.target;
     setSelected(value);
-  }
-  const handleChange =(e)=>{
-    const {name,value}=e.target;
-    setData(prevState=>({...prevState,[name]:value}))
-  }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     const isValid = validate();
     if (isValid) {
-      // dispatch({
-      //   type: registerActionTypes.ADD_REGISTER_DATA,
-      //   payload: {
-      //     agePreference: age,
-      //     occupationPreference: occupation,
-      //     genderPreference: selected,
-      //   },
-      // });
-      let data = register
-      data = {...data,
-                agePreference: age,
-                occupationPreference: occupation,
-                genderPreference: selected,
-              }
-      postData(data)
+      let data = register;
+      data = {
+        ...data,
+        agePreference: age,
+        occupationPreference: occupation,
+        genderPreference: selected,
+      };
+      postData(data);
       history.push("/dashboard");
     }
-  }
+  };
   const validate = () => {
     let isValid = true;
     if (age === "") isValid = false;
@@ -60,25 +48,29 @@ function Registration5() {
     return isValid;
   };
 
-  const postData = async(data)=>{
-    const uid = auth.currentUser.uid
+  const postData = async (data) => {
+    const uid = auth.currentUser.uid;
     const userRef = firestore.doc(`/users/${uid}`);
     const snapShot = await userRef.get();
     if (snapShot.exists) {
       try {
-        await userRef.set({
-         ...data,
-         isRegistered:true
-        },{merge:true});
+        await userRef.set(
+          {
+            ...data,
+            isRegistered: true,
+          },
+          { merge: true }
+        );
       } catch (e) {
         console.log("error creating user", e.message);
       }
     }
-  }
-  return <div className="registration-container">
-          <h1>Your Preferences</h1>
-         <form className="input-form" onSubmit={handleSubmit}>
-      <div className="name">
+  };
+  return (
+    <div className="registration-container">
+      <h1>Your Preferences</h1>
+      <form className="input-form" onSubmit={handleSubmit}>
+        <div className="name">
           <label htmlFor="age">Age</label>
           <input
             type="number"
@@ -92,14 +84,14 @@ function Registration5() {
           />
           <label htmlFor="bio">Any Preferences regarding your datae</label>
           <input
-          type="text"
+            type="text"
             placeholder="ex: #techie #banglore"
             name="occupation"
             id="occupation"
             value={occupation}
             onChange={handleChange}
           />
-           <h4>Popular Choices</h4>
+          <h4>Popular Choices</h4>
           <p>
             <span>#Product</span>
             <span>#Consultant</span>
@@ -114,15 +106,16 @@ function Registration5() {
             <span>#Business </span>
           </p>
         </div>
-        <label>When do you plan to settle on</label>
-          <CustomRadioButton
-            items={items}
-            selected={selected}
-            onClick={handleSelection}
-          />
+        <label>Gender</label>
+        <CustomRadioButton
+          items={items}
+          selected={selected}
+          onClick={handleSelection}
+        />
         <button>Submit</button>
       </form>
-  </div>;
+    </div>
+  );
 }
 
 export default Registration5;
