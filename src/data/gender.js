6bc025e6e7e1d687a0a620fc725abc41b8022c70
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-04-30 23:23:30 +0530

    add data list

---
+++ src/data/gender.js
@@ -0,0 +1,2 @@
+const genderList = [{label:"Male",value:1}, {label:"Female",value:2}, {label:"Other",value:3}]
+export default genderList
---
+++ src/data/settlement.js
@@ -0,0 +1,7 @@
+const settlementList = [{label:"6 months",value:1 },
+{label:"6 month - 1yr",value:2 },
+{label:"1yr - 2yr",value:3 },
+{label:"2+yrs",value:4 },
+{label:"No plans as of now",value:5 }
+];
+export default settlementList
