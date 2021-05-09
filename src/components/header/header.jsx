b75e05f166e287dc8ed13a8d965301f19999092e
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-05-09 05:36:04 +0530

    sign out button crrction

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,55 +1,11 @@
 import React from "react";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { useStateValue } from "../../StateProvider";
 import { auth } from "../../firebase/firebase.utils";
 import "./header.scss";
 import { Link } from "react-router-dom";
-import AuthenticationService from "../../services/authenticationservice";
-
-// function Header(props) {
-//   const [{ user }] = useStateValue();
-//   return (
-//     <div className="header">
-//       <Link to="/" className="logo-container">
-//         <Logo className="logo" />
-//       </Link>
-//       <div className="options">
-//         <Link className="option" to="/">
-//           HOME
-//         </Link>{" "}
-//         <Link className="option" to="/about">
-//           ABOUT US
-//         </Link>{" "}
-//         <Link className="option" to="/dashboard">
-//           DASHBOARD
-//         </Link>
-//         <Link className="option" to="/about">
-//           CONTACT US
-//         </Link>
-//         {user?.currentUser ? (
-//           <div
-//             className="option"
-//             onClick={() => {
-//               AuthenticationService.logout();
-//               auth.signOut();
-//             }}
-//           >
-//             SIGN OUT
-//           </div>
-//         ) : (
-//           <Link className="option" to="/login">
-//             SIGN IN
-//           </Link>
-//         )}
-//         <p>{user?.currentUser?.displayName}</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Header;
-
 function Header() {
+  const [{ user }] = useStateValue();
+
   return (
     <div class="nav">
       <input type="checkbox" id="nav-check" />
@@ -68,24 +24,26 @@ function Header() {
       {/* //--------HamBurger-Menu-Buton--------------- */}
 
       <div class="nav-links">
-        <Link to="/">HOME</Link> <Link to="/about">ABOUT US</Link>{" "}
-        <Link to="/dashboard">DASHBOARD</Link>
-        <Link to="/login">CONTACT US</Link>
-        {/* <a href="" target="">
-          Github
-        </a>
-        <a href="" target="">
-          Stackoverflow
-        </a>
-        <a href="" target="">
-          LinkedIn
-        </a>
-        <a href="" target="">
-          Codepen
-        </a>
-        <a href="" target="">
-          JsFiddle
-        </a> */}
+        <Link to="/" className="options">
+          HOME
+        </Link>{" "}
+        <Link to="/about" className="options">
+          ABOUT US
+        </Link>{" "}
+        {user?.currentUser && (
+          <Link to="/dashboard" className="options">
+            {user?.currentUser?.displayName}
+          </Link>
+        )}
+        {user?.currentUser ? (
+          <div className="options" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link to="/login" className="options">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
