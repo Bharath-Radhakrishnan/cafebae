8a2aa3e5a254e4d4e0b896adb877e250d4268ee0
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-05-16 18:49:25 +0530

    add vrtical tab and styles

--- src/tabs/dashboard/account-settings/account-settings.jsx
+++ src/tabs/dashboard/account-settings/account-settings.jsx
@@ -1,7 +1,8 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useDocument } from "react-firebase-hooks/firestore";
 import { auth, firestore } from "../../../firebase/firebase.utils";
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./account-settings.scss";
 function AccountSettings() {
   const [user] = useAuthState(auth);
 
@@ -19,27 +20,72 @@ function AccountSettings() {
     occupation,
   } = userSnapshot?.data();
   return (
-    <div>
+    <div className="account-settings-container">
       <h2>Any content 2</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>userName:{userName}</td>
-          </tr>
-          <tr>
-            <td>email:{email}</td>
-          </tr>
-          <tr>
-            <td>linkedin:{linkedInURL}</td>
-          </tr>
-          <tr>
-            <td>phoneno:{phoneNo}</td>
-          </tr>
-          <tr>
-            <td>occupation:{occupation}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Tabs>
+        <TabList>
+          <Tab>
+            <p>Title 1</p>
+          </Tab>
+          <Tab>
+            <p>Title 2</p>
+          </Tab>
+          <Tab>
+            <p>Title 3</p>
+          </Tab>
+          <Tab>
+            <p>Title 4</p>
+          </Tab>
+          <Tab>
+            <p>Title 5</p>
+          </Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="panel-content">
+            <h2>Any content 1</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>userName:{userName}</td>
+                </tr>
+                <tr>
+                  <td>email:{email}</td>
+                </tr>
+                <tr>
+                  <td>linkedin:{linkedInURL}</td>
+                </tr>
+                <tr>
+                  <td>phoneno:{phoneNo}</td>
+                </tr>
+                <tr>
+                  <td>occupation:{occupation}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>Any content 2</h2>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>Any content 3</h2>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>Any content 4</h2>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>Any content 5</h2>
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
