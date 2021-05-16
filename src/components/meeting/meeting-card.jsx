a752c1d9751f0e08c1f8ee218f02290529b76b6f
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-05-16 18:27:30 +0530

    style changes

--- src/components/meeting/meeting-card.jsx
+++ src/components/meeting/meeting-card.jsx
@@ -1,15 +1,32 @@
+import { Avatar } from "@material-ui/core";
+import EventIcon from "@material-ui/icons/Event";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+//-----------Style-Sheet----------
 import "./meeting-card.scss";
 function MeetingCard() {
   return (
     <div className="meeting-card-container">
-      <div>
-        <h3>Meeting</h3>
-        <p>Member1</p>
-        <p>Member2</p>
+      <div className="user-avatar-container">
+        <Avatar className="user-avatar">U</Avatar>
+      </div>
+      <div className="user-data-container">
+        <h4>Name</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
       </div>
       <div>
-        <p>Time</p>
-        <p>Link goes here</p>
+        <p>
+          <EventIcon />
+          Date
+        </p>
+        <p>
+          <ScheduleIcon />
+          Time
+        </p>
       </div>
     </div>
   );
