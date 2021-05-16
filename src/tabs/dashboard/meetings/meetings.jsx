143566fadb8ea3936fe4256be78e1f4ca5eeab87
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-05-16 13:05:57 +0530

    import meeting card

--- src/tabs/dashboard/meetings/meetings.jsx
+++ src/tabs/dashboard/meetings/meetings.jsx
@@ -1,8 +1,16 @@
+import MeetingCard from "../../../components/meeting/meeting-card";
+
 function Meetings() {
   return (
     <div>
       <h2>Any content 1</h2>
       List View
+      <div>
+        <MeetingCard />
+        <MeetingCard />
+        <MeetingCard />
+        <MeetingCard />
+      </div>
     </div>
   );
 }
