9da3e6021541a472620779c469378b207fda2565
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-06-21 00:31:55 +0530

    Modifications

--- src/components/header-tailwind/HeaderTailwind.js
+++ src/components/header-tailwind/HeaderTailwind.js
@@ -1,190 +1,21 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Transition } from "@headlessui/react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { useStateValue } from "../../StateProvider";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-
 import { Disclosure, Menu } from "@headlessui/react";
 import { BellIcon } from "@heroicons/react/outline";
 export default function HeaderTailwind() {
   const [{ user }] = useStateValue();
-
-  return header2(user);
-  // return header1(user);
-}
-const header1 = (user) => {
-  return (
-    <Popover className="relative bg-blue-100">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6">
-            <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-              {/* Logo */}
-              <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="/">
-                  <span className="sr-only">Workflow</span>
-                  <img
-                    className="h-8 w-auto sm:h-10"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt=""
-                  />
-                </a>
-              </div>
-              {/* Logo Ends */}
-              <div className="-mr-2 -my-2 md:hidden">
-                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                  <span className="sr-only">Open menu</span>
-                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                </Popover.Button>
-              </div>
-              <Popover.Group as="nav" className="hidden md:flex space-x-10">
-                <Link
-                  to="/"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Home
-                </Link>{" "}
-                <Link
-                  to="/about"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  About us
-                </Link>{" "}
-                {user?.currentUser && (
-                  <Link
-                    to="/dashboard"
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {user?.currentUser?.displayName}
-                  </Link>
-                )}
-              </Popover.Group>
-              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                {user?.currentUser ? (
-                  <div
-                    className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                    onClick={() => auth.signOut()}
-                  >
-                    SIGN OUT
-                  </div>
-                ) : (
-                  <Link
-                    to="/login"
-                    className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                  >
-                    Sign In
-                  </Link>
-                )}
-              </div>
-            </div>
-          </div>
-          {/*----------- Mobile-View------ */}
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="duration-200 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              static
-              className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-            >
-              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div className="pt-5 pb-6 px-5">
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                      />
-                    </div>
-                    <div className="-mr-2">
-                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                        <span className="sr-only">Close menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </Popover.Button>
-                    </div>
-                  </div>
-                </div>
-                <div className="py-6 px-5 space-y-6">
-                  <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                    <Link
-                      href="/"
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      Home
-                    </Link>
-                    <Link
-                      href="/about"
-                      className="text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
-                      About
-                    </Link>
-                    {user?.currentUser && (
-                      <Link
-                        to="/dashboard"
-                        className="text-base font-medium text-gray-900 hover:text-gray-700"
-                      >
-                        {user?.currentUser?.displayName}
-                      </Link>
-                    )}
-                    {user?.currentUser ? (
-                      <div className="options" onClick={() => auth.signOut()}>
-                        SIGN OUT
-                      </div>
-                    ) : (
-                      <Link to="/login" className="options">
-                        SIGN IN
-                      </Link>
-                    )}
-                  </div>
-                  <div>
-                    <a
-                      href="/"
-                      className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                    >
-                      Sign up
-                    </a>
-                    {/* <p className="mt-6 text-center text-base font-medium text-gray-500">
-                        Existing customer?{" "}
-                        <a
-                          href="/"
-                          className="text-indigo-600 hover:text-indigo-500"
-                        >
-                          Sign in
-                        </a>
-                      </p> */}
-                  </div>
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </>
-      )}
-    </Popover>
-  );
-};
-const header2 = (user) => {
-  /* This example requires Tailwind CSS v2.0+ */
-
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
-
   const navigation = [
-    { name: "Home", href: "/", current: true },
-    { name: "About", href: "/", current: false },
-    { name: "Projects", href: "/", current: false },
-    { name: "Calendar", href: "/", current: false },
+    { id: 1, name: "Home", route: "/", current: true },
+    { id: 2, name: "About", route: "/about", current: false },
   ];
+  const [active, setActive] = useState(0);
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -224,43 +55,25 @@ const header2 = (user) => {
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    <Link
-                      to="/"
-                      className={classNames(
-                        1
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "px-3 py-2 rounded-md text-sm font-medium"
-                      )}
-                      // aria-current={item.current ? "page" : undefined}
-                    >
-                      Home
-                    </Link>
-                    <Link
-                      to="/about"
-                      className=" text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                    >
-                      About
-                    </Link>
-                    <p className=" text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                      {user?.currentUser?.displayName}
-                    </p>
-
-                    {/* {navigation.map((item) => (
-                      <Link
-                        key={item.name}
-                        to={item.href}
-                        className={classNames(
-                          item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        // aria-current={item.current ? "page" : undefined}
-                      >
-                        {item.name}
-                      </Link>
-                    ))} */}
+                    {navigation.map((item, index) => {
+                      const isActive = active === index ? true : false;
+                      return (
+                        <Link
+                          key={item.name}
+                          to={item.route}
+                          onClick={() => setActive(index)}
+                          className={classNames(
+                            isActive
+                              ? "bg-gray-900 text-white"
+                              : " text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium hover:no-underline"
+                          )}
+                          aria-current={isActive ? "page" : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      );
+                    })}
                   </div>
                 </div>
               </div>
@@ -287,28 +100,43 @@ const header2 = (user) => {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
+              {navigation.map((item, index) => {
+                const isActive = active === index ? true : false;
+                return (
+                  <Link
+                    key={item.name}
+                    to={item.route}
+                    onClick={() => setActive(index)}
+                    className={classNames(
+                      isActive
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium hover:no-underline"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Link>
+                );
+              })}
+              <Link
+                to="/login"
+                className={classNames(
+                  0
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block px-3 py-2 rounded-md text-base font-medium hover:no-underline"
+                )}
+              >
+                Sign In
+              </Link>
             </div>
           </Disclosure.Panel>
         </>
       )}
     </Disclosure>
   );
-};
+}
 
 const CustomMenu = ({ user }) => {
   function classNames(...classes) {
