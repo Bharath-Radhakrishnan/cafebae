bd5c08fdd970b287bff9fbe980b5199b7627db04
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-06-28 20:41:23 +0530

    changes

--- src/components/meeting/meeting-card-tailwind.js
+++ src/components/meeting/meeting-card-tailwind.js
@@ -1,3 +1,5 @@
+import EventIcon from "@material-ui/icons/Event";
+import ScheduleIcon from "@material-ui/icons/Schedule";
 function MeetingCardTailwind() {
   return (
     <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
@@ -8,18 +10,28 @@ function MeetingCardTailwind() {
         width={384}
         height={512}
       />
-      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p className="text-lg font-semibold">
-            “Tailwind CSS is the only framework that I've seen scale on large
-            teams. It’s easy to customize, adapts to any design, and the build
-            size is tiny.”
-          </p>
-        </blockquote>
-        <figcaption className="font-medium">
-          <div className="text-cyan-600">Sarah Dayan</div>
-          <div className="text-gray-500">Staff Engineer, Algolia</div>
-        </figcaption>
+      <div className="pt-6 md:p-8 pr-16 text-left md:text-left space-y-4">
+        <div className="text-cyan-600">
+          <h5>Sarah Dayan</h5>
+        </div>
+        <div className="text-gray-500">Staff Engineer, Algolia</div>
+
+        <p className="text-lg font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <div className="pt-6 md:p-8 mr-24 text-left md:text-left space-y-4">
+        <p>
+          <EventIcon />
+          <span>Date</span>
+        </p>
+        <p>
+          <ScheduleIcon />
+          Time
+        </p>
       </div>
     </figure>
   );
