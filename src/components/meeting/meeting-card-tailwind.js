a65a1ad24e820f398af3f0d9987e95f92f0e761e
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-07-04 12:29:44 +0530

    add new img

--- src/components/meeting/meeting-card-tailwind.js
+++ src/components/meeting/meeting-card-tailwind.js
@@ -4,23 +4,25 @@ function MeetingCardTailwind() {
   return (
     <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
       <img
-        className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-        src="/logo.png"
+        className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto object-cover"
+        src="/user-img-1.jpeg"
         alt=""
         width={384}
         height={512}
       />
-      <div className="pt-6 md:p-8 text-left md:text-left space-y-4">
+      <div className="pt-6 md:p-8 text-left">
         <div className="text-cyan-600">
           <h5>Sarah Dayan</h5>
         </div>
-        <div className="text-gray-500">Staff Engineer, Algolia</div>
+        <div className="text-gray-500">
+          <p>Staff Engineer, Algolia</p>
+        </div>
 
         <p className="text-lg font-semibold">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          {/* Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. */}
         </p>
       </div>
       <div className="pt-6 md:p-8 mr-24 text-left md:text-left space-y-4">
