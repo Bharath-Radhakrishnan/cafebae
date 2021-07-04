2f220f7bd44aaf3ebc325741f1f3ee6210a675b0
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-07-04 10:58:29 +0530

    rm header

--- src/tabs/dashboard/meetings/meetings.jsx
+++ src/tabs/dashboard/meetings/meetings.jsx
@@ -3,7 +3,6 @@ import MeetingCardTailwind from "../../../components/meeting/meeting-card-tailwi
 function Meetings() {
   return (
     <div>
-      <h2>Upcoming Meetings</h2>
       <div>
         <MeetingCardTailwind />
         <MeetingCardTailwind />
