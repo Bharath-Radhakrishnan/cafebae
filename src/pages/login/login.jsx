8f0db13ead83669021f4fca84c76eb11cf308fc3
Bharath-Radhakrishnan <bharath.radhakrishnan16@gmail.com>
2021-08-09 23:12:07 +0530

    sign in with gapi method

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { signInWithGoogle, gapiSignIn } from "../../firebase/firebase.utils";
 import { useHistory } from "react-router-dom";
 
 import "./login.scss";
 function Login() {
   const history = useHistory();
   const signIn = async () => {
-    const auth = await signInWithGoogle();
+    // const auth = await signInWithGoogle();
+    const auth = await gapiSignIn();
+
     if (auth) {
       history.replace("/dashboard");
     }
